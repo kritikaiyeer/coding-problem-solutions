77136208ccd586d5f3810f04a2e4566690907b33
Krithika Iyer <gokrithikaiyer4@gmail.com>
2021-12-28 18:08:53 -0800

    Update SetMatrixZero.java

--- SetMatrixZero.java
+++ SetMatrixZero.java
@@ -31,3 +31,31 @@ class Solution {
 }
 
 // Time complexity = O(mn) Brute force
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row[] = new int[matrix.length];
+        int col[] = new int[matrix[0].length];
+     
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j] == 0){
+                   row[i] = -1;
+                   col[j] = -1;
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(row[i] == -1 || col[j] == -1){
+                   matrix[i][j] = 0;
+                }
+            }
+        }
+        
+    }
+}
+
+// Time complexity O(N*M + N*M) Space Complexity O(N) + O(M) - Better
+
+
