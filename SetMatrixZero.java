d6b05c7a9a38d24e1b2bc34aa868f3e74de450a9
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-01-06 12:36:24 -0800

    Update SetMatrixZero.java

--- SetMatrixZero.java
+++ SetMatrixZero.java
@@ -1,8 +1,8 @@
 // Set Matrix Zero
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int row[] = new int[matrix.length];
-        int col[] = new int[matrix[0].length];
+        int row = matrix.length;
+        int col = matrix[0].length;
      
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
@@ -30,7 +30,7 @@ class Solution {
     }
 }
 
-// Time complexity = O(mn) Brute force
+// Time complexity = O((N*M)*(N + M)). O(N*M) Brute force Space Complexity - O(1)
 
 class Solution {
     public void setZeroes(int[][] matrix) {
@@ -58,4 +58,25 @@ class Solution {
 
 // Time complexity O(N*M + N*M) Space Complexity O(N) + O(M) - Better
 
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int col0 = 1,rows = matrix.length, cols = matrix[0].length; 
+        
+        for(int i = 0; i < rows; i++){
+            if(matrix[i][0] == 0) col0 = 0;
+            for(int j = 1;j<cols;j++)
+                if(matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+        
+        for(int i = rows-1; i>=0;i--){
+            for(int j = cols-1;j>=1;j--)
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if(col0 == 0) matrix[i][0] = 0;
+            
+        }
+    }
+}
 
+// Time complexity O(NM) Space Complexity O(1)
