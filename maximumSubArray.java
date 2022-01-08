46ad14e693f9641f0c07236b1f6e2a64fe1b2918
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-01-07 18:51:54 -0800

    Create maximumSubArray.java

---
+++ maximumSubArray.java
@@ -0,0 +1,11 @@
+// Kadane's Algorithm
+// Find the maximum sum of a sub array of given array
+// Brute force - O(N3)
+// initialize maximum = 0
+// three loops -
+// 1. first loop 0 - n-1
+// 2. second loop 0 - n -1
+// 3. third loop i - j 
+//     here find sum of array elements from i - j
+// store the value if greater than max.
+// return the max value
