c37f50fd21a862195acdcbdc9fca419001e095dc
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-01-07 19:29:28 -0800

    Update maximumSubArray.java

--- maximumSubArray.java
+++ maximumSubArray.java
@@ -9,3 +9,44 @@
 //     here find sum of array elements from i - j
 // store the value if greater than max.
 // return the max value
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                int sum = 0;
+                for(int k = i;k<=j;k++){
+                    sum+=nums[k];
+                }
+                if(sum > max){
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+}
+
+// Above doesnt work for all negative nums.
+
+// Better approach O(N2)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        for(int i=0;i<nums.length;i++){
+            int sum = 0;
+            for(int j=i;j<nums.length;j++){
+                sum+= nums[j];
+                if(sum > max){
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+}
+
+// has some problems as well
+
+// Optimal
