ad788872d7dd7d09c275081f7b738a4cbf63cfc3
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-01-07 18:33:51 -0800

    Update nextPermutation.java

--- nextPermutation.java
+++ nextPermutation.java
@@ -21,3 +21,26 @@
 
 // Time complexity - O(N) + O(N) + O(N) = O(3N) = O(N)
 // Space Complexity - O(1)
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while(i >=0 && nums[i] >= nums[i+1]) i--;
+        if(i>=0){
+            int j = nums.length - 1;
+            while(j>=0 && nums[j] <= nums[i]) j--;
+            swap(nums, i,j);
+        }
+        reverse(nums,i+1,nums.length-1);
+    }
+    
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public void reverse(int[] nums, int i, int j){
+        while(i < j) swap(nums,i++,j--);
+    }
+}
