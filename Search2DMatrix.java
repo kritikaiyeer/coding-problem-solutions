6752cc159946b7255e49a670594fd395b4602afb
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-01-24 18:49:55 -0500

    Update Search2DMatrix.java

--- Search2DMatrix.java
+++ Search2DMatrix.java
@@ -24,4 +24,24 @@ class Solution {
     }
 }
 
-// Time Complexity - O(N*logn) 
+// Time Complexity - O(N*logm) 
+
+class Solution {
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[0].length -1;
+        while(j>= 0 && i < matrix.length ){
+            if(target == matrix[i][j]){
+                return true;
+            }else if(target < matrix[i][j]){
+                j--;
+            } else if(target > matrix[i][j]){
+                i++;
+            }
+        }
+        return false;
+    }
+}
+
+// Time complexity - 0(N)
