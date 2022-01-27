3d800b6d383133945c044d0def2e1a57887f2dd8
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-01-26 23:53:34 -0500

    Update majorityElement-II.java

--- majorityElement-II.java
+++ majorityElement-II.java
@@ -20,3 +20,52 @@ class Solution {
 }
 
 // TC: O(NLOGN)
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+        List<Integer> result = new ArrayList<>();
+        int num1 = -1;
+        int num2 = -1;
+        int c1 = 0;
+        int c2 = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == num1){
+                c1++;
+            }else if(nums[i] == num2){
+                c2++;
+            }else if(c1 == 0){
+                num1 = nums[i];
+                c1 =1;
+            } else if(c2 == 0){
+                num2 = nums[i];
+                c2 = 1;
+            } else{
+                c1--;
+                c2--;
+            }
+        }
+        c1 = 0;
+        c2 = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == num1){
+                c1++;
+            }
+            if(nums[i] == num2){
+                c2++;
+            }
+        }
+        if(c1 > nums.length/3){
+            result.add(num1);
+        }
+        if(c2 > nums.length/3){
+           if(num1 != num2){
+               result.add(num2);
+           }
+        }
+        
+        return result;
+    }
+}
+
+// TC: O(N) + O(N) SP: O(1)
