a3c2d3695b93e705594c7be936841fae5134a709
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-01-27 00:40:54 -0500

    Update gridUniquePaths.java

--- gridUniquePaths.java
+++ gridUniquePaths.java
@@ -17,3 +17,27 @@ class Solution {
 
 // recursive and doesnt work for lot of test cases
 // TC: exponential
+
+class Solution {
+    public int countPaths(int i, int j,int n, int m,int[][] dp){
+       
+        if(i == (n-1) && j == (m -1)){
+            return 1;
+        }
+         if(i >= n || j>= m ){
+            return 0;
+        } 
+        if(dp[i][j] != -1) return dp[i][j];
+        else return dp[i][j] = countPaths(i+1,j,n,m,dp) + countPaths(i,j+1,n,m,dp);
+    }
+    
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int[] row: dp)
+            Arrays.fill(row, -1);
+        return countPaths(0,0,m,n,dp);
+    }
+}
+
+// convert recursive method to dp by storing the result of each recursive call and reducing sub overlaps
+// TC: (N*M) SP: (N*M)
