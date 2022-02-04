859040f969eeceb27896bd6262d2ca8d09d58200
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-02-04 15:51:06 -0500

    Create rotateArrayByKTimes.java

---
+++ rotateArrayByKTimes.java
@@ -0,0 +1,27 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int[] solution(int[] A, int K) {
+        int[] res = new int[A.length];
+        int j = K;
+        while(j != 0){
+            res = new int[A.length];
+            for(int i=0;i<A.length;i++){
+                if(i == A.length - 1){
+                    res[0] = A[i];
+                }else{                    
+                    res[i+1] = A[i];
+                }    
+            }
+            A = res;
+            j--;
+        }
+         return res;   
+    }    
+  
+}
+
