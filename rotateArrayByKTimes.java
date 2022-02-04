4c233c627d13070ac4f7b1ed881d3cb286f947f9
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-02-04 16:17:51 -0500

    Update rotateArrayByKTimes.java

--- rotateArrayByKTimes.java
+++ rotateArrayByKTimes.java
@@ -28,4 +28,24 @@ class Solution {
     }    
   
 }
+// optimal approach reverse whole matrix, then start to k-1 and then k to length
+class Solution {
+    
+    public static void reverseArray(int[] nums, int start, int end){
+        while (start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }   
+    
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverseArray(nums,0,nums.length-1);
+        reverseArray(nums,0,k-1);
+        reverseArray(nums,k,nums.length-1);
+    }
+}
 
