448cf5d77ad08a9562cfb34d68d9cd26adef105a
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-02-12 00:37:27 -0500

    Update minAvgTwoSlice.java

--- minAvgTwoSlice.java
+++ minAvgTwoSlice.java
@@ -8,16 +8,16 @@ class Solution {
     public int solution(int[] A) {
         // write your code in Java SE 8
         int minIndex = 0;
-        float min = Integer.MAX_VALUE;
-        for(int i=1;i<A.length-2;i++){
-           float avg1 = (float)(A[i] + A[i+1] + A[i+2])/3;
-           float avg2 = (float)(A[i] + A[i+1])/2;
-           if(Math.min(avg1,avg2) < min){
+        double min = Integer.MAX_VALUE;
+        for(int i=0;i<A.length-2;i++){
+           double avg1 = (double)(A[i] + A[i+1] + A[i+2])/3;
+           double avg2 = (double)(A[i] + A[i+1])/2;
+           if(avg1 < min || avg2 <min){
                min = Math.min(avg1,avg2);
                minIndex = i;
            }
         }
-        float avg1 = (float)(A[A.length-1] + A[A.length-2])/2;
+        double avg1 = (double)(A[A.length-1] + A[A.length-2])/2;
         if(avg1 < min){
             min = avg1;
             minIndex = A.length-2;
