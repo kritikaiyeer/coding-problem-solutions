93707fd485d84771da5702861ee925e3c618f3b3
Krithika Iyer <gokrithikaiyer4@gmail.com>
2022-03-10 22:08:17 -0500

    Update Best-Time-Buy-sell-stock.java

--- Best-Time-Buy-sell-stock.java
+++ Best-Time-Buy-sell-stock.java
@@ -1,24 +1,14 @@
-import java.util.Arrays;
- 
 class Solution {
-    public int arbit(int[] prices){
-        if(prices.length <= 2){
-            if(prices.length == 2 && prices[1] > prices[0]){
-                return prices[1] - prices[0];
-            }
-             return 0;
-        }
-        int m = prices.length/2;
-        int[] left = Arrays.copyOfRange(prices, 0, m);
-        int[] right = Arrays.copyOfRange(prices, m+1, prices.length);
-
-        int lg = arbit(left);
-        int rg = arbit(right);
-        int max = IntStream.of(left).max().orElse(Integer.MIN_VALUE);
-        int min = IntStream.of(right).min().orElse(Integer.MAX_VALUE);
-        return Math.max(Math.max(lg, rg), max-min);
-    }
     public int maxProfit(int[] prices) {
-        return arbit(prices);
+        int maxProfit = 0; 
+        int min_val = Integer.MAX_VALUE; 
+        for(int i = 0; i<prices.length; i++){
+            if(prices[i] < min_val){
+                min_val = prices[i]; 
+            }else if(prices[i]-min_val > maxProfit){
+                maxProfit = prices[i] - min_val; 
+            }
+        }
+        return maxProfit; 
     }
 }
